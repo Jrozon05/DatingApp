8ffdce51ef0061f359559ac48e2523a72ab0bc5d
Jordani Rozon <jrozon@pecopallet.com>
2019-11-26 20:40:18 -0500

    Changing Specific values to properties in the automapper profile

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using DatingApp.API.DTOs;
 using DatingApp.API.Models;
@@ -8,8 +9,16 @@ namespace DatingApp.API.Helpers
     {
         public AutoMapperProfiles()
         {
-            CreateMap<User, UserForListDto>();
-            CreateMap<User, UserForDetailedDto>();
+            CreateMap<User, UserForListDto>()
+                .ForMember(destination => destination.PhotoUrl, 
+                            options => options.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(destionation => destionation.Age, 
+                            options => options.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(destination => destination.PhotoUrl, 
+                            options => options.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(destionation => destionation.Age, 
+                            options => options.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotoForDetailedDto>();
         }
     }
